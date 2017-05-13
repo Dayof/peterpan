0a3d86a3e492ee7d833706f4b1e954cdfbaa1f24
Day <dayannefernandesc@gmail.com>
2017-05-13 20:30:35 -0300

    change default port [2]

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from peterpan import app
 
 if __name__ == '__main__':
-    app.run()
-# 
+    app.run(host='0.0.0.0', port=8080)
+#
 # from flask import Flask, Blueprint, render_template
 # app = Flask(__name__)
 #
