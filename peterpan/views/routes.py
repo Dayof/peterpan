25b13799a0923bd078827afdb062a21f77cfcb8c
imota <mota.icaro@gmail.com>
2017-05-13 19:42:22 -0300

    Getting links for all urls on the page now

--- peterpan/views/routes.py
+++ peterpan/views/routes.py
@@ -8,20 +8,29 @@ links = ['http://g1.globo.com/','http://www.google.com/']
 labels = ['G1', 'Google']
 
 def isUrlValid(url):
-	return (not url is None) and url.startswith('https://')
+	return (not url is None)
+
+def isUrlIncomplete(url):
+	return not url.startswith("http://")
 
 @mod.route('/')
 def index():
 
 	pages = requests.get('http://g1.globo.com/')
-	soup = BeautifulSoup(pages.content, 'html.parser')
-	a_link = soup.find_all('a')
-	
-	for link in a_link:
-		url = link.get('href')
-		if isUrlValid(url):
-			links.append(url)
-			labels.append(url)
 
-	content = zip(labels, links)
-	return render_template('links_list.html', content=content)
+	if pages.status_code == 200:
+		soup = BeautifulSoup(pages.content, 'html.parser')
+		a_link = soup.find_all('a')
+
+		for link in a_link:
+			url = link.get('href')
+			if isUrlValid(url):
+				if isUrlIncomplete(url):
+					url = 'http://g1.globo.com' + url
+				links.append(url)
+				labels.append(url)
+
+		content = zip(labels, links)
+		return render_template('links_list.html', content=content)
+	else:
+		return render_template('404.html')
