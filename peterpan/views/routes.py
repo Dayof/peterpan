13e09ff9fcabe7f8665c3047bc35d6a241f05fad
imota <mota.icaro@gmail.com>
2017-05-13 18:00:41 -0300

    Added a template for displaying links to websited in a html

--- peterpan/views/routes.py
+++ peterpan/views/routes.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 mod = Blueprint('main', __name__)
 
+links = ['http://g1.globo.com/','http://www.google.com/']
+label = ['G1', 'Google']
+
 @mod.route('/')
 def index():
-    return render_template('home.html')
+	content = zip(label, links)
+	return render_template('links_list.html', content=content)
