6a06e479e6978ec63d10b88923cf08cbdf0e5830
imota <mota.icaro@gmail.com>
2017-05-13 19:25:53 -0300

    Getting links for the g1 page

--- peterpan/views/routes.py
+++ peterpan/views/routes.py
@@ -1,11 +1,27 @@
 from flask import Blueprint, render_template
+from bs4 import BeautifulSoup
+import requests
 
 mod = Blueprint('main', __name__)
 
 links = ['http://g1.globo.com/','http://www.google.com/']
-label = ['G1', 'Google']
+labels = ['G1', 'Google']
+
+def isUrlValid(url):
+	return (not url is None) and url.startswith('https://')
 
 @mod.route('/')
 def index():
-	content = zip(label, links)
+
+	pages = requests.get('http://g1.globo.com/')
+	soup = BeautifulSoup(pages.content, 'html.parser')
+	a_link = soup.find_all('a')
+	
+	for link in a_link:
+		url = link.get('href')
+		if isUrlValid(url):
+			links.append(url)
+			labels.append(url)
+
+	content = zip(labels, links)
 	return render_template('links_list.html', content=content)
