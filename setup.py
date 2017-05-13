c3dac1f9222cd0bd3490dbe0cf860b6de16b83a8
imota <mota.icaro@gmail.com>
2017-05-13 19:52:05 -0300

    Added requests to the setup.py file

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=['Flask',
-                    'beautifulsoup4']
+                    'beautifulsoup4', 'requests']
 )
