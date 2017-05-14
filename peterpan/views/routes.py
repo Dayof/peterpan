f10cee800938ab2898e67a72c35d5fa24569eddc
imota <mota.icaro@gmail.com>
2017-05-13 21:21:13 -0300

    Redirecting to the first page found

--- peterpan/views/routes.py
+++ peterpan/views/routes.py
@@ -1,36 +1,48 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 from bs4 import BeautifulSoup
 import requests
 
 mod = Blueprint('main', __name__)
 
-links = ['http://g1.globo.com/','http://www.google.com/']
-labels = ['G1', 'Google']
+links = []
+labels = []
 
 def isUrlValid(url):
-	return (not url is None)
+	return not (url is None)
 
-def isUrlIncomplete(url):
-	return not url.startswith("http://")
+def isUrlComplete(url):
+	return url.startswith("//g1.globo.com/busca")
+
+def canConnect(page):
+	return page.status_code == 200
+
+def add_new(url):
+	url = 'http:' + url
+	page = requests.get(url)
+
+	if canConnect(page):
+		#soup = BeautifulSoup(page.content, 'html.parser')
+		links.append(url)
+		labels.append(url)
 
 @mod.route('/')
 def index():
+	termo_busca = 'enem'
+	pages = requests.get('http://g1.globo.com/busca/?q=' + termo_busca)
 
-	pages = requests.get('http://g1.globo.com/')
-
-	if pages.status_code == 200:
+	if canConnect(pages):
 		soup = BeautifulSoup(pages.content, 'html.parser')
 		a_link = soup.find_all('a')
 
+		html_content = ''
 		for link in a_link:
 			url = link.get('href')
 			if isUrlValid(url):
-				if isUrlIncomplete(url):
-					url = 'http://g1.globo.com' + url
-				links.append(url)
-				labels.append(url)
+				if isUrlComplete(url):
+					add_new(url)
 
 		content = zip(labels, links)
+		return redirect(links[0],code=302)
 		return render_template('links_list.html', content=content)
 	else:
 		return render_template('404.html')
